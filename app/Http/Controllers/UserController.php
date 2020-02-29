a331caa57215b8bd513bcb97884b52b237fa6b2e
Aryawan Wijaya <nikolaus.wijaya@si.ukdw.ac.id>
2020-02-29 20:17:48 +0700

    edit+create user_JWT

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,160 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use JWTAuth;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Illuminate\Support\Facades\DB;
+class UserController extends Controller
+{
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        try {
+            if (! $token = JWTAuth::attempt($credentials)) {
+                return response()->json(['error' => 'invalid_credentials'], 400);
+            }
+        } catch (JWTException $e) {
+            return response()->json(['error' => 'could_not_create_token'], 500);
+        }
+        $role = DB::table('users')->where('email',$request->email)->pluck('role')->first();
+        return response()->json([
+            'token' => $token,
+            'role' => $role
+        ]);
+    }
+
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6|confirmed',
+        ]);
+
+        if($validator->fails()){
+            return response()->json($validator->errors()->toJson(), 400);
+        }
+
+        $user = User::create([
+            'name' => $request->get('name'),
+            'email' => $request->get('email'),
+            'password' => Hash::make($request->get('password')),
+            'role' =>1,
+            'status' =>'AUDISI',
+            'count_voute' =>0,
+
+        ]);
+
+        $token = JWTAuth::fromUser($user);
+
+        return response()->json(compact('user','token'),201);
+    }
+
+    public function registerJuri(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6|confirmed',
+        ]);
+
+        if($validator->fails()){
+            return response()->json($validator->errors()->toJson(), 400);
+        }
+
+        $user = User::create([
+            'name' => $request->get('name'),
+            'email' => $request->get('email'),
+            'password' => Hash::make($request->get('password')),
+            'role' =>2,
+            'status' =>'AKTIF',
+            'count_voute' =>null,
+        ]);
+        $token = JWTAuth::fromUser($user);
+
+        return response()->json(compact('user','token'),201);
+    }
+
+    public function registerAdmin(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6|confirmed',
+        ]);
+
+        if($validator->fails()){
+            return response()->json($validator->errors()->toJson(), 400);
+        }
+
+        $user = User::create([
+            'name' => $request->get('name'),
+            'email' => $request->get('email'),
+            'password' => Hash::make($request->get('password')),
+            'role' =>0,
+            'status' =>'ADMIN',
+            'count_voute' =>null,
+        ]);
+
+
+        $token = JWTAuth::fromUser($user);
+
+        return response()->json(compact('user','token'),201);
+    }
+
+    public function getAuthenticatedUser()
+    {
+        try {
+
+            if (! $user = JWTAuth::parseToken()->authenticate()) {
+                return response()->json(['user_not_found'], 404);
+            }
+
+        } catch (Tymon\JWTAuth\Exceptions\TokenExpiredException $e) {
+
+            return response()->json(['token_expired'], $e->getStatusCode());
+
+        } catch (Tymon\JWTAuth\Exceptions\TokenInvalidException $e) {
+
+            return response()->json(['token_invalid'], $e->getStatusCode());
+
+        } catch (Tymon\JWTAuth\Exceptions\JWTException $e) {
+
+            return response()->json(['token_absent'], $e->getStatusCode());
+
+        }
+
+        return response()->json(compact('user'));
+    }
+
+    public function readAllDataPeserta(){
+        $user=DB::table('users')->get();
+        return response()->json(compact('user'),200);
+    }
+
+    public function editDataPeserta(Request $request, $id){
+       $user= DB::table('users')->where('id',$id)->update([
+        'name' => $request->get('name'),
+        'email' => $request->get('email'),
+        'password' => Hash::make($request->get('password')),
+        ]);
+        $user = User::find($id);
+        return response()->json(compact('user'),200);
+    }
+
+    public function editDataJuri(Request $request, $id){
+        $user= DB::table('users')->where('id',$id)->update([
+         'name' => $request->get('name'),
+         'email' => $request->get('email'),
+         'status' => $request->get('status'),
+         ]);
+         $user = User::find($id);
+         return response()->json(compact('user'),200);
+     }
+}
--- routes/api.php
+++ routes/api.php
@@ -18,10 +18,26 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::post('/admin/createPeserta', 'adminController@createPeserta');
-Route::post('/admin/updatePeserta', 'adminController@updatePeserta');
-Route::get('/admin/getAllPeserta', 'adminController@readAllData');
-Route::post('/admin/getOnePeserta', 'adminController@readOneData');
-Route::post('/admin/deletePeserta', 'adminController@deletePeserta');
+
+//peserta --> dilakukan sendiri oleh peserta
+Route::post('/registerPeserta', 'UserController@register');
+Route::post('/pesertaEdit/{id}','UserController@editDataPeserta')->middleware('jwt.verify');
+
+//admin
+Route::post('/registerAdmin', 'UserController@registerAdmin');
+//juri --> dilakukan oleh admin
+Route::post('/registerJuri', 'UserController@registerJuri')->middleware('jwt.verify');
+Route::post('/juriEdit/{id}','UserController@editDataJuri')->middleware('jwt.verify');
+
+//umum
+Route::post('/login', 'UserController@login');
+Route::get('/user', 'UserController@getAuthenticatedUser')->middleware('jwt.verify');
+Route::get('/allPeserta', 'UserController@readAllDataPeserta')->middleware('jwt.verify');
+
+// Route::post('/admin/createPeserta', 'adminController@createPeserta');
+// Route::post('/admin/updatePeserta', 'adminController@updatePeserta');
+// Route::get('/admin/getAllPeserta', 'adminController@readAllData')->middleware('jwt.verify');
+// Route::post('/admin/getOnePeserta', 'adminController@readOneData');
+// Route::post('/admin/deletePeserta', 'adminController@deletePeserta');
 
 
